34b84a35a5c9652ca5f7d73070088d285d24b604
Carlos Martinez <martinezcj2@gmail.com>
2018-06-12 18:46:02 -0400

    Write tests fro comment box

---
+++ src/components/__test__/CommentBox.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+
+import CommentBox from 'components/CommentBox';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+})
+
+it('has a text area and a button', () => {
+  expect(wrapped.find('textarea').length).toEqual(1);
+  expect(wrapped.find('button').length).toEqual(1);
+});
+
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+    wrapped.update();
+  });
+
+  it('has a text area that users can type', () => {  
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
+  
+  it('has a text area that clears out when form is submitted', () => { 
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
+});
