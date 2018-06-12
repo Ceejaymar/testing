47e79ce9069a726a58758de5e1d81ac2ba84498f
Carlos Martinez <martinezcj2@gmail.com>
2018-06-12 01:16:12 -0400

    Create form for comment box

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-  return <div>Comment Box</div>
+class CommentBox extends Component {
+  state = { comment: '' };
+
+  handleChange = event => {
+    this.setState({ comment: event.target.value })
+  } 
+
+  onSubmit = event => {
+    event.preventDefault();
+
+    this.setState({ comment: '' });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a comment</h4>
+        <textarea onChange={this.handleChange} value={this.state.comment} name="" id="" cols="30" rows="10" />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    )
+  }
 }
+
+export default CommentBox;
