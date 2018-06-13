debf37a62f6577bcac459ee631ff80610f40fb4b
Carlos Martinez <martinezcj2@gmail.com>
2018-06-12 23:40:29 -0400

    Remove provider and import from Root

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from 'reducers';
-
+import Root from 'Root';
 import App from 'components/App';
 
-render(<App />,
-document.querySelector('#root'))
+render(
+  <Root>
+    <App />
+  </Root>,
+  document.querySelector('#root')
+)
